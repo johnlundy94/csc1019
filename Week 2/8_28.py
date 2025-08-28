cd28fa7fef16647dd15be733c3de437360bf1a66
John Lundy <johnclundyy@gmail.com>
2025-08-28 12:10:58 -0600

    8_28

---
+++ Week 2/8_28.py
@@ -0,0 +1,50 @@
+int = type(10)
+float = type(3.3)
+name = "John"
+300.25
+print(int)
+print(float)
+print(name)
+
+# input_name = input("What is my name ")
+# print(input_name)
+
+fruits = ["apple", "banana", "cherry"]
+grades = [90,85, 88]
+
+print(fruits)
+print(grades)
+
+number = 10
+text = str(number)
+change_int = int(text)
+
+print(text)
+print(type(text))
+print(change_int)
+print(type(change_int))
+
+# age = input("How old are you? ")
+# print("you are " + age + " years old")
+# print("you are " , age , " years old")
+
+food = ["pizza", "sushi", "ice cream"]
+print(food)
+print(food[0])
+print(food[1])
+print(food[2])
+# print(food[3])
+
+# print("My age is" + 25)
+print(f"My age is {25}")
+print("----------------------------------")
+print("----------------------------------")
+print("----------------------------------")
+
+name = input("What is your name? ")
+hobby = input("What is your hobby? ")
+print(f"Hello {name}, your hobby is {hobby}!")
+input("Press enter to exit...")
+
+
+
